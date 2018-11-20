5a9e3b2b0d86dbea6c0826c77b84af27b7f0b0e9
KhalyavinSergey <44732519+KhalyavinSergey@users.noreply.github.com>
2018-11-20 20:16:18 +0300

    Add files via upload

---
+++ zadacha 7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacha_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = 1;
+
+            double s = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                s += 24 / (9 * (Math.Pow(n, 2)) - 12 * n - 5);
+
+
+            }
+            Console.WriteLine("Sum = {0}", s);
+            Console.ReadLine();
+        }
+    }
+}
